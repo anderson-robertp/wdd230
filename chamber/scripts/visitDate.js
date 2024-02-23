bdf4575ae277b31912d9bd428e8ead2c3b73cb8b
anderson-robertp <Anderon-RP@hotmail.com>
2024-02-23 11:52:06 -0600

    get dates

---
+++ chamber/scripts/visitDate.js
@@ -0,0 +1,25 @@
+const dateDisp = document.querySelector("#date");
+
+let today = new Date();
+console.log('today: '+ today);
+
+let lastVisit = Date(window.localStorage.getItem("dateVisit-ls")) || 0;
+console.log('last visit: ' + lastVisit)
+
+let sinceLastVisit = today.getTime() - lastVisit;
+console.log('Since last visit: ' + sinceLastVisit)
+
+if (sinceLastVisit  <= 86400000) {
+    dateDisp.textContent = 'Back so soon! Awesome!';
+} else if(sinceLastVisit > 86400000) {
+    numDays = sinceLastVisit / 86400000
+    if (numDays > 1) {
+        dateDisp.textContent = 'You last visited ' + numDays + ' days ago.'
+    } else {
+        dateDisp.textContent = 'You last visited 1 day ago.'
+    }
+} else {
+    dateDisp.textContent = 'Welcome! Let us know if you have any questions.';
+}
+
+localStorage.setItem("dateVisit-ls",today);
