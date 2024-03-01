83878cc91deefb29e241780a198670dc4a41fb1c
anderson-robertp <anderon-rp@hotmail.com>
2024-02-29 22:25:25 -0600

    wk8 thurs update 2

---
+++ scripts/forms.js
@@ -0,0 +1,19 @@
+const pw1 = document.querySelector("#password");
+const pw2 = document.querySelector("#password2");
+const message = document.querySelector("#formmessage");
+
+pw2.addEventListener("focusout", checkSame);
+
+function checkSame() {
+	if (pw1.value !== pw2.value) {
+		message.textContent = "❗Passwords DO NOT MATCH!";
+		message.style.visibility = "show";
+		pw2.style.backgroundColor = "#fff0f3";
+		pw2.value = "";
+		pw2.focus();
+	} else {
+		message.style.display = "none";
+		pw2.style.backgroundColor = "#fff";
+		pw2.style.color = "#000";
+	}
+}
