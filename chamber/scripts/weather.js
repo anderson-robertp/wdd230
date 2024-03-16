e0bd61639503b9d1126408707adf4d187f4ee826
anderson-robertp <anderon-rp@hotmail.com>
2024-03-16 15:37:22 -0500

    firecats update

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -4,12 +4,21 @@ const wind = document.querySelector('#windSpeed')
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption')
 const eventSection = document.querySelector('#weather-card')
+const daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
 
 
 //32.1632756377172, -91.72025644128209
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=32.16&units=imperial&lon=-91.72&appid=6b753c5b3f206d70be293add3e8903f6';
 const directionURL = 'https://anderson-robertp.github.io/wdd230/chamber/data/directions.json'
-const threeURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=32.16&lon=-91.72&units=imperial&appid=6b753c5b3f206d70be293add3e8903f6'
+const threeURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=32.16&lon=-91.72&units=imperial&cnt=32&appid=6b753c5b3f206d70be293add3e8903f6'
 
 async function apiFetch(url) {
     try {
@@ -65,9 +74,10 @@ async function fetchThreeDay(url) {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            console.log(data)
+            //console.log(data)
             //displayResults(data);
-            return data
+            displayThreeDay(data)
+            //return data
         } else {
             throw Error(await response.text());
         }
@@ -77,10 +87,75 @@ async function fetchThreeDay(url) {
     }
 }
 
-function diplayThreeDay(data) {
+function displayThreeDay(data) {
+    //console.log(data);
+    const threeTemp = data.list[0].main.temp.toFixed(0);
+    const threeHumid = data.list[0].main.humidity;
+    const threeWind = data.list[0].wind.speed;
+    const threeDeg = data.list[0].wind.deg;
+    const today = findDay(data.list[0]);
+    const cDay = today;
+    //console.log(`temp: ${threeTemp} + Humid:${threeHumid} + wind: ${threeWind} + deg: ${threeDeg}`);
+    //console.log(`day: ${today} + ms:${cDay}`);
+    // Display events
+    //console.log(data.list)
+    // Calc three day forecast
+    var conData = {};
+    for (let i = 0; i < data.list.length; i++) {
+        const entry = data.list[i];
+        //console.log(entry)
+        const entryDay = findDay(entry);
+        //console.log(entryDay);
+        if (!conData[entryDay]) {
+            conData[entryDay] = {
+                low: entry.main.temp_min,
+                high: entry.main.temp_max,
+                desc: entry.weather[0].description,
+                icon: entry.weather[0].icon,
+                //
+            }
+        } else {
+            if(entryDay.temp_min < conData[entryDay].low) {
+                conData[entryDay].low = entryDay.temp_min
+            }
+            if (entryDay.temp_max > conData[entryDay].high){
+                conData[entryDay].high = entryDay.temp_max
+            }
+        }
+        
+    }
+
+    console.log(conData);
+
+    let first = 1
+    for (const indexDay in conData){
+        if (first == 1 || first == 5) {
+            first++;
+            console.log(first)
+            continue;
+        } else {
+            const tempHigh = conData[indexDay].high;
+            const tempLow = conData[indexDay].low;
+            const foreDesc = conData[indexDay].desc;
+            const foreIcon = conData[indexDay].icon;
+            
+            console.log(`${first} + ${indexDay} + ${tempHigh} + ${tempLow} + ${foreDesc} + ${foreIcon}`)
+        }
+        first++
+
+    }
+    
     
 }
 
+function findDay(data) {
+    var ms = data.dt*1000;
+    const date = new Date(ms);
+    const day = date.getDay()
+    const nameday = daysOfWeek[day];
+    //console.log(nameday);
+    return nameday
+}
 
 apiFetch(url);
 fetchThreeDay(threeURL);
