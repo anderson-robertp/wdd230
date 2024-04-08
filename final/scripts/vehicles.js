d405f30b6f4930cf42d567de4df650654cfbefa0
anderson-robertp <anderon-rp@hotmail.com>
2024-04-07 23:16:25 -0500

    rental card update

--- final/scripts/vehicles.js
+++ final/scripts/vehicles.js
@@ -4,7 +4,7 @@ const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
 const tablebutton = document.querySelector("#table");
 const display = document.querySelector("article");
-const table = document.querySelector('#dataTable tbody')
+//const table = document.querySelector('#dataTable tbody')
 
 gridbutton.addEventListener("click", showGrid);
 listbutton.addEventListener("click", showList);
@@ -28,31 +28,118 @@ function parseData(array) {
     const atvs = array.vehicles.atvs
     const jeeps = array.vehicles.jeeps
     console.table(scooters);
+    createCards(scooters);
+    createCards(atvs);
+    createCards(jeeps);
 }
 
 function populateTable(array) {
+    const table = document.querySelector('#datatable tbody')
     console.table(array);
-    const make = array.make;
-    const model = array.model;
-    const resHalf = array.reserve.resHalf;
-    const resFull = array.reserve.resFull;
-    const walkHalf = array.walk-in.walkHalf;
-    const walkFull = array.reserve.walkFull;
-    console.log(`${make} ${model} ${resHalf} ${resFull} ${walkHalf} ${walkFull}`)
+    const scooters = array.vehicles.scooters
+    const atvs = array.vehicles.atvs
+    const jeeps = array.vehicles.jeeps
+    const rowScoot = document.createElement('tr');
+    rowScoot.innerHTML = `
+        <td>Scooters</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    `;
+    table.appendChild(rowScoot);
+    tableRow(scooters,table);
+    const rowATV = document.createElement('tr');
+    rowATV.innerHTML = `
+        <td>ATVs</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    `;
+    table.appendChild(rowATV);
+    tableRow(atvs,table);
+    const rowJeep = document.createElement('tr');
+    rowJeep.innerHTML = `
+        <td>Jeeps</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    `;
+    table.appendChild(rowJeep);
+    tableRow(jeeps,table);
+    
 }
 
 function createCards(array) {
-    const make = array.make;
-    const model = array.model;
-    const specs = array.specs;
-    const capacity = array.capacity;
-    const pic = array.pic;
-    
+    array.forEach(element => {
+        const make = element.make;
+        const model = element.model;
+        const specs = element.specs;
+        const capacity = element.capacity;
+        const pic = element.pic;
+        console.log(`${make} `)
+        // create section
+        const section = document.createElement('section');
+        // create pic and img element
+        const picElm = document.createElement('picture')
+        const img = document.createElement('img')
+        // set img attributes
+        img.setAttribute('src',pic)
+        img.setAttribute('alt',`logo of ${make} ${model} ${specs}`)
+        img.setAttribute('loading','lazy')
+        // append img to pic
+        picElm.appendChild(img);
+        // Create H3
+        const h3 = document.createElement('h3')
+        h3.textContent = `${make} ${model} ${specs}`
+        // Text content for h3 will be make and model
+        // Create p element
+        const p = document.createElement('p');
+        // Text for p will be specs and capacity
+        p.textContent = `Capacity ${capacity}`
+        // append all items to section
+        section.appendChild(picElm)
+        section.appendChild(h3)
+        section.appendChild(p)
+        // append section to article
+        display.appendChild(section)
+    });
+}
+
+function tableRow(array,table){
+    //const table = document.querySelector('#datatable tbody')
+    array.forEach(element => {
+        const make = element.make;
+        const model = element.model;
+        const resHalf = element.reserve[0].resHalf;
+        const resFull = element.reserve[1].resFull;
+        const walkHalf = element.walkin[0].walkHalf;
+        const walkFull = element.walkin[1].walkFull;
+        console.log(`${make} ${model} ${resHalf} ${resFull} ${walkHalf} ${walkFull}`)
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td></td>
+            <td>${make}</td>
+            <td>${model}</td>
+            <td>${resHalf}</td>
+            <td>${resFull}</td>
+            <td>${walkHalf}</td>
+            <td>${walkFull}</td>
+        `;
+        table.appendChild(row);
+    })
 }
 
 function showList() {
     display.classList.remove("grid");
-    display.classList.remove("table");
     display.classList.add("list");
 }
 
